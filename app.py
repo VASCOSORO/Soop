2bf34c6a30afd476721b0ea69845c1bca6bf16ec
VASCOSORO <virtualgraf00@gmail.com>
2024-09-26 19:24:05 +0000

    Eliminar token del archivo app.py

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
 import streamlit as st
 import pandas as pd
+import os
+
+excel_file_url = 'https://raw.githubusercontent.com/VASCOSORO/Soop/main/1083%20productos%20al%2024%20de%20sep.xlsx'
+
+df = pd.read_excel(excel_file_url)
 
 st.title('Super Buscador de Productos')
 
-# Ruta del archivo Excel en tu Google Drive o entorno local
-excel_file_path = '/content/drive/MyDrive/Colab Notebooks/1083 productos al 24 de sep.xlsx'
-
-# Cargar el archivo Excel
-df = pd.read_excel(excel_file_path)
-
-# Mostrar el contenido del Excel
 st.write("Contenido del archivo Excel:")
 st.dataframe(df)
 
-# Agregar funcionalidad de búsqueda si es necesario
 producto_buscado = st.text_input("Buscar producto por nombre:")
 
 if producto_buscado:
     resultados = df[df['Nombre'].str.contains(producto_buscado, case=False, na=False)]
     st.write(f"Resultados para '{producto_buscado}':")
     st.dataframe(resultados)
+
+# Luego lo subimos a GitHub:
+!git add app.py
+!git commit -m "Subir el archivo app.py"
