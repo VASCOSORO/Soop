6bd8061a4b443e9f711fc7aba9f10ef6bb11a2f7
VASCOSORO <virtualgraf00@gmail.com>
2024-09-28 06:32:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -186,4 +186,7 @@ st.markdown("""
             position: fixed;
             left: 0;
             bottom: 0;
-           
+            width: 100%;
+            background-color: transparent;
+            color: black;
+            text
