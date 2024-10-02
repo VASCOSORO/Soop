35832fcaab7cc48fbe8aececb665d803e3ff119e
VASCOSORO <virtualgraf00@gmail.com>
2024-10-02 17:53:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from io import BytesIO
 from datetime import datetime
 import pytz  # Para manejar zonas horarias
 
+# ====== Version 2.0.1 ==== Funcionando 
 # Definir la zona horaria de Argentina
 tz_argentina = pytz.timezone('America/Argentina/Buenos_Aires')
 
@@ -95,10 +96,10 @@ def mostrar_producto_completo(producto, mostrar_mayorista, mostrar_descuento, de
             else:
                 st.write("Imagen no disponible.")
     with col_btns:
-        st.markdown("**Tamaño**")
-        if st.button("🔼 Aumentar"):
+        st.markdown("**Vista**")
+        if st.button("➕"):
             st.session_state.img_size = min(st.session_state.get('img_size', 300) + 50, 600)
-        if st.button("🔽 Disminuir"):
+        if st.button("➖"):
             st.session_state.img_size = max(st.session_state.get('img_size', 300) - 50, 100)
 
     # Checkbox para mostrar ubicación
