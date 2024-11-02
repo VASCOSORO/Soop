7db792b205553d91b869f844b1f4ca739486656d
VASCOSORO <virtualgraf00@gmail.com>
2024-11-02 02:45:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,15 +13,16 @@ tz_argentina = pytz.timezone('America/Argentina/Buenos_Aires')
 # Función para obtener la fecha de la última modificación del archivo en GitHub
 def obtener_fecha_modificacion_github(usuario, repo, archivo):
     url = f"https://api.github.com/repos/{usuario}/{repo}/commits?path={archivo}&per_page=1"
-    response = requests.get(url)
-    if response.status_code == 200:
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
         commit_data = response.json()[0]
         fecha_utc = commit_data['commit']['committer']['date']
         fecha_utc = datetime.strptime(fecha_utc, "%Y-%m-%dT%H:%M:%SZ")
         fecha_argentina = fecha_utc.astimezone(tz_argentina)
         return fecha_argentina.strftime("%Y-%m-%d %H:%M:%S")
-    else:
-        return "No se pudo obtener la fecha de actualización"
+    except:
+        return None
 
 # Detalles del repositorio
 usuario = "VASCOSORO"
@@ -106,7 +107,11 @@ mostrar_seccion_superior = st.checkbox("Mostrar detalles de archivo y botón de 
 if mostrar_seccion_superior:
     st.success("Archivo cargado correctamente.")
     st.markdown("<hr>", unsafe_allow_html=True)
-    st.markdown(f"<p style='font-size: 12px;'>Última modificación del archivo {archivo}: {fecha_ultima_modificacion}</p>", unsafe_allow_html=True)
+    
+    # Mostrar la fecha de última modificación si está disponible
+    if fecha_ultima_modificacion:
+        st.markdown(f"<p style='font-size: 12px;'>Última modificación del archivo {archivo}: {fecha_ultima_modificacion}</p>", unsafe_allow_html=True)
+    
     st.success(f"Se cargaron {df.shape[0]} filas y {df.shape[1]} columnas del archivo de Excel.")
 
     # Checkbox para activar la subida de un nuevo archivo
@@ -286,4 +291,4 @@ if ordenar_por_novedad:
 
 # Footer
 st.markdown("<hr>", unsafe_allow_html=True)
-st.markdown("<p style='text-align: center; font-size: 12px;'>Powered by VASCO.SORO</p>", unsafe_allow_html=True) 
+st.markdown("<p style='text-align: center; font-size: 12px;'>Powered by VASCO.SORO</p>", unsafe_allow_html=True)
